58fe5964e41b12f296593bfb5f594975cd5060b9
Adilson Fernando <ornellas00@hotmail.com>
2023-07-04 11:57:49 -0300

    feat: adicionando o projeto

---
+++ src/main/Main.js
@@ -0,0 +1,89 @@
+import React, {useEffect, useState} from 'react';
+import {v4 as uuidV4} from 'uuid';
+import "./Main.scss";
+import Sublista from './sublista/sublista';
+
+import imgaddlista from './assets/buttonaddlista.svg'
+import iconlist from './assets/iconlist.svg'
+import deletelist from './assets/excluirlista.svg'
+
+
+function Main() {
+  // const listasInicial=[
+  //   {
+  //     id:'1',
+  //     titulo:'Lista de Desejos',
+  //   },
+  //   {
+  //     id:'2',
+  //     titulo:'Tarefas',
+  //   },
+  //   {
+  //     id:'3',
+  //     titulo:'Receitas',
+
+  //   },
+  // ];
+  const dataLocalStorage = JSON.parse(localStorage.getItem('listas')||'[]');
+  const [lista,setLista] = useState(dataLocalStorage);
+  const [novaListaInicial,setNovaListaInicial] = useState('');
+
+  function addNovoItem (event){
+    event.preventDefault();
+    setLista([...lista, { id: uuidV4(), titulo:novaListaInicial}]);
+    setNovaListaInicial('');
+  }
+  function removeItem (id){
+    setLista(lista.filter(lista=>lista.id !== id))
+  }
+  useEffect(()=> {
+    localStorage.setItem('lista', JSON.stringify(lista));
+  },[lista])
+
+  return (
+    <div className='main'>
+      <div className='mainconteiner'>
+
+      <div className='maininputbutton'>
+
+        <input 
+          className='imputmain'
+          value={novaListaInicial}
+          type="text" 
+          placeholder='Qual lista você deseja criar?' 
+          onChange={(event) => setNovaListaInicial(event.target.value)}
+          />
+
+        <button className='buttonmain' onClick={addNovoItem}>
+            <img className='imgbutton' src={imgaddlista} alt="button adicionar lista" />
+        </button>
+
+        </div>  
+      </div>
+
+      <ul className='ullista'>
+        <div className='ulconteiner'>
+          {lista.map(listasInicial=>(
+            <li className='lilista' key={listasInicial.id}>
+              <div className='conteinerli'>
+
+              <img className='iconlist' src={iconlist} alt="icon List" />
+              {listasInicial.titulo}
+                <div className='conteinerdeleteli'>
+                  <button className='deleteibutton' onClick={() => removeItem(listasInicial.id)}>
+                    <img className='deleteicon' src={deletelist} alt="Excluir Lista" />
+                  </button>
+                </div>
+              </div>
+            <div className='sublistconteiner'>
+              <Sublista></Sublista>
+            </div>
+          </li>))}
+        </div>
+      </ul>
+
+    </div>
+  );
+}
+
+export default Main;
---
+++ src/main/sublista/sublista.js
@@ -0,0 +1,134 @@
+import React, {useEffect, useState } from 'react';
+import { v4 as uuidV4 } from 'uuid';
+import './sublista.scss';
+
+import buttonadd from './assets/buttonaddsub.svg'
+import buttoneditar from './assets/buttoneditar.svg'
+import buttondeletar from './assets/buttonexcluir.svg'
+
+import { DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
+
+function Sublista() {
+  const dataLocalStorage = JSON.parse(localStorage.getItem('subListas') || '[]');
+  const [subLista, setsubLista] = useState(dataLocalStorage);
+  const [novaSubListaInicial, setNovaSubListaInicial] = useState('');
+  const [idEditado, setIdEditado] = useState(''); 
+  const [tituloEditado, setTituloEditado] = useState(''); 
+
+  function addNovoItem(event) {
+    event.preventDefault();
+    setsubLista([...subLista, { id: uuidV4(), titulo: novaSubListaInicial }]);
+    setNovaSubListaInicial('');
+  }
+
+  function editarItem(id) {
+    const itemEditado = subLista.find(item => item.id === id);
+    if (itemEditado) {
+      setTituloEditado(itemEditado.titulo);
+      setIdEditado(id);
+    } else {
+      setIdEditado('');
+      setTituloEditado('');
+    }
+  }
+
+  function salvarEdicao(id) {
+    setsubLista(subLista.map(item => {
+      if (item.id === id) {
+        return { ...item, titulo: tituloEditado };
+      }
+      return item;
+    }));
+    setIdEditado('');
+    setTituloEditado('');
+  }
+
+  function removeItem(id) {
+    setsubLista(subLista.filter(subLista => subLista.id !== id));
+  }
+
+  useEffect(() => {
+    localStorage.setItem('subLista', JSON.stringify(subLista));
+  }, [subLista]);
+
+ 
+  function onDragEnd(result) {
+    if (!result.destination) {
+      return;
+    }
+
+    const items = Array.from(subLista);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    setsubLista(items);
+  }
+
+
+  return (
+    <div className='subconteiner'>
+      <div className='conteinerinput'>
+        <input
+          className='inputsublist'
+          value={novaSubListaInicial}
+          type="text"
+          placeholder="Adicione sub-itens a sua lista"
+          onChange={event => setNovaSubListaInicial(event.target.value)}
+        />
+
+        <button className='buttonaddsub' onClick={addNovoItem}>
+          <img className='imgbuttonadd' src={buttonadd} alt="button adicionar" />
+        </button>
+      </div>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId={subLista}>
+          {(provided) => (
+            <div className='ulconteiner'>
+              <ul className='subLista' {...provided.droppableProps} ref={provided.innerRef}>
+                {provided.placeholder}
+                {subLista.map((subListasInicial, index)=> (
+                  <Draggable key={subListasInicial.id} draggableId={subListasInicial.id} index={index}>
+                    {(provided) => (
+                      <li className='lisublista' key={subListasInicial.id}
+                      ref={provided.innerRef}
+                      {...provided.draggableProps} 
+                      {...provided.dragHandleProps}>
+                        {subListasInicial.id === idEditado ? (
+                          <>
+                            <input
+                              className='imputedit'
+                              type="text"
+                              value={tituloEditado}
+                              onChange={event => setTituloEditado(event.target.value)}
+                              />
+                            <button className='buttonsalvaredit' onClick={() => salvarEdicao(subListasInicial.id)}>Salvar</button>
+                          </>
+                        ) : (
+                          <>
+                            {subListasInicial.titulo}
+                            <button className='buttoneditarisublist' onClick={() => editarItem(subListasInicial.id)}>
+                              <img className='imgbuttoneditar' src={buttoneditar} alt="Editar Item Sub Lista" />
+
+                            </button>
+                          </>
+                        )}
+                        <div className='conteinerdeletarsubitem'>
+                          <button className='deletarsublist' onClick={() => removeItem(subListasInicial.id)}>
+                            <img className='imgbuttondeletar' src={buttondeletar} alt="Excluir item sub Lista" />
+                          </button>
+                        </div>
+                      </li>
+                    )}
+                  </Draggable>
+                ))}
+              </ul>
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+    </div>
+  );
+}
+
+export default Sublista;
+
